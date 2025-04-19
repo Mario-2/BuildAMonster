d8131ccbd14d606016934f6aca84e9a54e1513ef
Mario-2 <67210496+Mario-2@users.noreply.github.com>
2025-04-18 23:08:19 -0700

    Update Monster.js

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -31,15 +31,53 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        my.sprite.leg_l = this.add.sprite(this.bodyX - 60, this.bodyY + 100, "monsterParts", "leg_blueC.png");
+        my.sprite.leg_l.flipX = true;
+        my.sprite.leg_r = this.add.sprite(this.bodyX + 60, this.bodyY + 100, "monsterParts", "leg_blueC.png");
+        my.sprite.arm_l = this.add.sprite(this.bodyX - 90, this.bodyY + 60, "monsterParts", "arm_blueE.png");
+        my.sprite.arm_l.flipX = true;
+        my.sprite.arm_r = this.add.sprite(this.bodyX + 90, this.bodyY + 60, "monsterParts", "arm_blueE.png");
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_redD.png");
+        my.sprite.mouth_smile = this.add.sprite(this.bodyX, this.bodyY + 40, "monsterParts", "mouth_closed_happy.png");
+        my.sprite.mouth_fang = this.add.sprite(this.bodyX, this.bodyY + 40, "monsterParts", "mouth_closed_fangs.png");
+        my.sprite.mouth_fang.visible = false;
+        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY - 20, "monsterParts", "eye_human_blue.png");
+        my.sprite.horn_l = this.add.sprite(this.bodyX - 50, this.bodyY - 70, "monsterParts", "detail_white_horn_small.png");
+        my.sprite.horn_l.flipX = true;
+        my.sprite.horn_r = this.add.sprite(this.bodyX + 50, this.bodyY - 70, "monsterParts", "detail_white_horn_small.png");
 
-        
+        this.dkey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        this.akey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.fkey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
+        this.skey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
 
-       
+        // polling input: movement
+        if(this.dkey.isDown){
+            for(let image in my.sprite) {
+                my.sprite[image].x += 0.2;
+            }
+        }
+        if(this.akey.isDown){
+            for(let image in my.sprite) {
+                my.sprite[image].x -= 0.2;
+            }
+        }
+
+        //Event input: dimple smile
+        this.fkey.on('down', (key, event) => {
+            my.sprite.mouth_fang.visible = true;
+            my.sprite.mouth_smile.visible = false;
+        })
+
+        //Event input: regular smile
+        this.skey.on('down', (key, event) => {
+            my.sprite.mouth_smile.visible = true;
+            my.sprite.mouth_fang.visible = false;
+        })
     }
 
 }
